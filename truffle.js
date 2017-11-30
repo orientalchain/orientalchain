e73a7bbeee7452b78ead0ce945c1294868aa3240
OrientalChain <OrientalChain>
2017-11-30 16:37:09 +0800

    ERC20 Token
    Powerful ERC20 Token

---
+++ truffle.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "build": "webpack",
+  "deploy": [
+    "OrientalChainToken"
+  ],
+  "rpc": {
+    // Default RPC configuration.
+    "host": "localhost",
+    "port": 8545
+  }
+}
